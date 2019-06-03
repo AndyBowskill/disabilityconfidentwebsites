b5e9eb7b605417f57f491d8863304ca1f984de28
Andy Bowskill <andybowskill@gmail.com>
2019-06-03 12:54:07 +0100

    Test for the company website.

--- test_main.py
+++ test_main.py
@@ -6,6 +6,19 @@ class TestFindDisabilityConfidentWebsite(unittest.TestCase):
     def setUp(self):
         self.find_website = main.FindDisabilityConfidentWebsite()
 
+    def test_process_csv_company_website(self):
+        isValid = self.find_website.process_csv_company_website('https://www.abc.com')
+        self.assertTrue(isValid)
+
+        isValid = self.find_website.process_csv_company_website(' ')
+        self.assertFalse(isValid)
+
+        isValid = self.find_website.process_csv_company_website('...')
+        self.assertFalse(isValid)
+
+        isValid = self.find_website.process_csv_company_website('https://www.youtube.com')
+        self.assertFalse(isValid)
+
     def test_is_row_valid(self):
         row = ['zero', 'one', 'two', 'three', 'four', 'five']
         isValid = self.find_website.is_row_valid(row)
